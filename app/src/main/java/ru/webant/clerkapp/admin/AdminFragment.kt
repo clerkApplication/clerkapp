8ae895510119479f1a04f565be75486bac643e21
kx30 <nikolay199808@gmail.com>
2020-02-17 22:50:15 +0300

    Вёрстка экрана панели администратра

--- app/src/main/java/ru/webant/clerkapp/admin/AdminFragment.kt
+++ app/src/main/java/ru/webant/clerkapp/admin/AdminFragment.kt
@@ -1,12 +1,9 @@
 package ru.webant.clerkapp.admin
 
-import android.content.Context
 import android.os.Bundle
-import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.arellomobile.mvp.MvpAppCompatActivity
 import com.arellomobile.mvp.MvpAppCompatFragment
 import com.arellomobile.mvp.presenter.InjectPresenter
 import ru.webant.clerkapp.R
